c363a53c631bcc9b4a83b7f9e0a0b641349c0a58
Yagmur Sahin <yagmurs@microsoft.com>
2022-12-01 15:13:14 +0300

    updated ubuntu image

--- arm/CustomScript/bootstrap.sh
+++ arm/CustomScript/bootstrap.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+## unattended-upgrade 
+apt-get update -y 
+unattended-upgrades --verbose
+
+## install azure cli
 sudo apt-get update
 sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
 curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
@@ -5,3 +12,15 @@ AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
 sudo apt-get update
 sudo apt-get install azure-cli
+
+## IP Forwarding
+sed -i -e 's/#net.ipv4.ip_forward.*/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
+sed -i -e 's/#net.ipv6.conf.all.forwarding.*/net.ipv6.conf.all.forwarding=1/g' /etc/sysctl.conf
+sysctl -p
+
+## Install WireGurard
+add-apt-repository ppa:wireguard/wireguard -y 
+apt-get update -y 
+apt-get install linux-headers-$(uname -r) -y
+apt-get install wireguard -y
+
